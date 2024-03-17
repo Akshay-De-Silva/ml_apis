632cb9ccca3c81d25f064ee27c95e80696d9f029
Akshay <akshaydesilva2002@gmail.com>
2024-03-17 14:36:21 +0530

    added ml code + requirements + gunicorn

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import ml
 
 app = Flask(__name__)
 CORS(app)
@@ -10,28 +11,9 @@ def getLogReg():
     weight_e = data.get('weight_e')
     weight_p = data.get('weight_p')
 
-    response = {
-        "BATI": weight_e,
-        "RAF1_CPU" : weight_e,
-        "RAF1_GPU" : weight_e,
-        "RAF1_RAM" : weight_e,
-        "F1CI" : weight_e,
-        "BRMSETI" : 0,
-        "RARS_CPU" : 0,
-        "RARS_GPU" : 0,
-        "RARS_RAM" : 0,
-        "RMSPECI" : 0,
-        "Accuracy" : weight_p,
-        "F1Score" : weight_p,
-        "Precision" : weight_p,
-        "Recall" : weight_p,
-        "MSE" : 0,
-        "RSME" : 0,
-        "MAE" : 0,
-        "R2" : 0
-    }
+    results = ml.getLogReg(weight_e, weight_p)
 
-    return jsonify(response)
+    return jsonify(results)
 
 @app.route('/getLinReg', methods=['POST'])
 def getLinReg():
@@ -39,28 +21,9 @@ def getLinReg():
     weight_e = data.get('weight_e')
     weight_p = data.get('weight_p')
 
-    response = {
-        "BATI": 0,
-        "RAF1_CPU" : 0,
-        "RAF1_GPU" : 0,
-        "RAF1_RAM" : 0,
-        "F1CI" : 0,
-        "BRMSETI" : weight_e,
-        "RARS_CPU" : weight_e,
-        "RARS_GPU" : weight_e,
-        "RARS_RAM" : weight_e,
-        "RMSPECI" : weight_e,
-        "Accuracy" : 0,
-        "F1Score" : 0,
-        "Precision" : 0,
-        "Recall" : 0,
-        "MSE" : weight_p,
-        "RSME" : weight_p,
-        "MAE" : weight_p,
-        "R2" : weight_p
-    }
+    results = ml.getLinReg(weight_e, weight_p)
 
-    return jsonify(response)
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ ml.py
@@ -0,0 +1,202 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error, accuracy_score, f1_score, confusion_matrix, precision_score, recall_score
+from statsmodels.tools.eval_measures import rmspe
+import timeit
+import psutil
+import json
+
+def reg_to_json(rmspeci, brmseti, rars_cpu, rars_gpu, rars_ram, mse, rmse, mae, r2):
+    output = {
+        "BATI": 0,
+        "RAF1_CPU" : 0,
+        "RAF1_GPU" : 0,
+        "RAF1_RAM" : 0,
+        "F1CI" : 0,
+        "BRMSETI" : round(brmseti,2),
+        "RARS_CPU" : round(rars_cpu,2),
+        "RARS_GPU" : round(rars_gpu,2),
+        "RARS_RAM" : round(rars_ram,2),
+        "RMSPECI" : round(rmspeci,2),
+        "Accuracy" : 0,
+        "F1Score" : 0,
+        "Precision" : 0,
+        "Recall" : 0,
+        "MSE" : round(mse,2),
+        "RSME" : round(rmse,2),
+        "MAE" : round(mae,2),
+        "R2" : round(r2,2)
+    }
+    return json.dumps(output, indent=4)
+
+def class_to_json(f1_cost_index, bati, raf1_cpu, raf1_gpu, raf1_ram, accuracy, f1Score, precision, recall):
+    output = {
+        "BATI": round(bati,2),
+        "RAF1_CPU" : round(raf1_cpu,2),
+        "RAF1_GPU" : round(raf1_gpu,2),
+        "RAF1_RAM" : round(raf1_ram,2),
+        "F1CI" : round(f1ci,2),
+        "BRMSETI" : 0,
+        "RARS_CPU" : 0,
+        "RARS_GPU" : 0,
+        "RARS_RAM" : 0,
+        "RMSPECI" : 0,
+        "Accuracy" : round(accuracy,2),
+        "F1Score" : round(f1Score,2),
+        "Precision" : round(precision,2),
+        "Recall" : round(recall,2),
+        "MSE" : 0,
+        "RSME" : 0,
+        "MAE" : 0,
+        "R2" : 0
+    }
+    return json.dumps(output, indent=4)
+
+def getLogReg(weight_external, weight_performance):
+    #Importing Dataset
+    classDataset = "https://raw.githubusercontent.com/Akshay-De-Silva/ml_apis/main/heart.csv"
+    dataset = pd.read_csv(classDataset)
+
+    #relabel values in columns to be numeric
+    label_encoder = LabelEncoder()
+    dataset['Sex'] = label_encoder.fit_transform(dataset['Sex'])
+    dataset['ChestPainType'] = label_encoder.fit_transform(dataset['ChestPainType'])
+    dataset['RestingECG'] = label_encoder.fit_transform(dataset['RestingECG'])
+    dataset['ExerciseAngina'] = label_encoder.fit_transform(dataset['ExerciseAngina'])
+    dataset['ST_Slope'] = label_encoder.fit_transform(dataset['ST_Slope'])
+
+    X=dataset[['Age', 'Sex', 'ChestPainType', 'RestingBP', 'Cholesterol',
+            'FastingBS', 'RestingECG', 'MaxHR', 'ExerciseAngina', 'Oldpeak', 'ST_Slope']].values
+    y=dataset[['HeartDisease']].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    y_train = y_train.ravel()
+    y_test = y_test.ravel()
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    # Logistic Regression
+    from sklearn.linear_model import LogisticRegression
+
+    logistic_regression = LogisticRegression()
+    training_time = timeit.timeit(lambda: logistic_regression.fit(X_train, y_train), number=1)
+    testing_time = timeit.timeit(lambda: logistic_regression.predict(X_test), number=1)
+
+    ram_usage_bytes = psutil.virtual_memory().used
+    ram_usage = ram_usage_bytes / (1024 ** 2)
+
+    y_pred = logistic_regression.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    f1Score = f1_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+
+    max_time = 3
+    min_time = 0.005
+    max_gpu = 2000
+    min_gpu = 1000
+    max_cpu = 30
+    min_cpu = 15
+    max_ram = 2000
+    min_ram = 1000
+
+    max_acc = 0.97
+    min_acc = 0.75
+    max_f1 =0.95
+    min_f1 = 0.7
+
+    gpu_usage = 1100
+    cpu_usage = 18
+
+    tp_cost = 1/13
+    tn_cost = 1/13
+    fp_cost = 4/13
+    fn_cost = 7/13
+
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    tp, fp, fn, tn = conf_matrix.ravel()
+    f1ci = (2*tp_cost*tp)/(2*tp_cost*tp + fp_cost*fp + fn_cost*fn)
+
+    bati = (weight_performance * ((accuracy - min_acc) / (max_acc - min_acc))) \
+            + (weight_external * (1 - ((training_time - min_time) / (max_time - min_time))))
+            
+    raf1_cpu = (weight_performance * ((f1Score-min_f1)/(max_f1-min_f1)) + (weight_external * (1-((cpu_usage-min_cpu)/(max_cpu-min_cpu)))))
+    raf1_gpu = (weight_performance * ((f1Score-min_f1)/(max_f1-min_f1)) + (weight_external * (1-((gpu_usage-min_gpu)/(max_gpu-min_gpu)))))
+    raf1_ram = (weight_performance * ((f1Score-min_f1)/(max_f1-min_f1)) + (weight_external * (1-((ram_usage-min_ram)/(max_ram-min_ram)))))
+
+    output = class_to_json(f1ci, bati, raf1_cpu, raf1_gpu, raf1_ram, accuracy, f1Score, precision, recall)
+    return output
+
+
+def getLinReg(weight_external, weight_performance):
+    #Importing Dataset
+    reg_dataset = 'https://raw.githubusercontent.com/Akshay-De-Silva/ml_apis/main/bitcoin.csv'
+    dataset=pd.read_csv(reg_dataset)
+
+    dataset = dataset.dropna()
+
+    X = dataset[['Open', 'High', 'Low', 'Close', 'Volume_(BTC)', 'Volume_(Currency)']]
+    y = dataset[['Weighted_Price']]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    y_train = np.ravel(y_train)
+    y_test = np.ravel(y_test)
+
+    # Linear Regression
+    from sklearn.linear_model import LinearRegression
+
+    linear_model = LinearRegression()
+    training_time = timeit.timeit(lambda: linear_model.fit(X_train_scaled, y_train), number=1)
+    testing_time = timeit.timeit(lambda: linear_model.predict(X_test_scaled), number=1)
+
+    ram_usage_bytes = psutil.virtual_memory().used
+    ram_usage = ram_usage_bytes / (1024 ** 2)
+
+    y_pred = linear_model.predict(X_test_scaled)
+
+    mse = mean_squared_error(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    rmse = np.sqrt(mse)
+    rmspe = rmspe(y_test, y_pred)
+
+    max_time = 0.024
+    min_time = 0.008
+    max_gpu = 2000
+    min_gpu = 1000
+    max_cpu = 30
+    min_cpu = 15
+    max_ram = 2000
+    min_ram = 1000
+
+    max_rmse = 5.14
+    min_rmse = 0.19
+    max_r2 = 1
+    min_r2 = 0.98
+
+    gpu_usage = 1100
+    cpu_usage = 18
+
+    cost = 5000
+
+    rmspeci = cost * rmspe
+
+    brmseti = (weight_performance * (1-((rmse - min_rmse) / (max_rmse - min_rmse)))) \
+            + (weight_external * (1 - ((training_time - min_time) / (max_time - min_time))))
+
+    rars_cpu = (weight_performance * ((r2-min_r2)/(max_r2-min_r2)) + (weight_external * (1-((cpu_usage-min_cpu)/(max_cpu-min_cpu)))))
+    rars_gpu = (weight_performance * ((r2-min_r2)/(max_r2-min_r2)) + (weight_external * (1-((gpu_usage-min_gpu)/(max_gpu-min_gpu)))))
+    rars_ram = (weight_performance * ((r2-min_r2)/(max_r2-min_r2)) + (weight_external * (1-((ram_usage-min_ram)/(max_ram-min_ram)))))
+
+    output = reg_to_json(rmspeci, brmseti, rars_cpu, rars_gpu, rars_ram, mse, rmse, mae, r2)
+    return output
